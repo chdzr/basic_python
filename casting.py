95e47099a58707abdec04d53e9d3a75e1defe38f
chdzr <chidzir.wicaksono@gmail.com>
2021-01-09 14:32:11 +0700

    Pertemuan ke-1

---
+++ casting.py
@@ -0,0 +1,12 @@
+#casting adalah merubah tipe data satu ke tipe data lain
+#casting dari float ke int akan selalu menjadi round down 
+x = int(2.8)
+y = float("3.0")
+z = str(2.7)
+
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
---
+++ data_types.py
@@ -0,0 +1,9 @@
+x = 5
+y = 4.5
+z = "Hello World"
+o = True
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(o))
