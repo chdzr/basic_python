da949b6bf9eee53882cadba9d74f303e81805be0
chdzr <chidzir.wicaksono@gmail.com>
2021-01-10 15:10:55 +0700

    add Tugas-1

---
+++ Tugas-1/Soal-1.py
@@ -0,0 +1,5 @@
+name = input("Masukkan Nama : ")
+age = int(input("Masukkan Umur : "))
+height = float(input("Masukkan Tinggi : "))
+
+print ("Nama saya {}, umur saya {} tahun dan tinggi saya {} cm.".format(name, age, height))
---
+++ Tugas-1/Soal-2.py
@@ -0,0 +1,4 @@
+radius = int(input("Masukkan Jari-Jari :"))
+pi = 22/7
+circleArea = float(pi * (radius ** 2)) 
+print("Luas lingkaran dengan jari-jari {} cm adalah {:.2f} cm\u00b2".format(radius,circleArea))
