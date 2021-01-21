161b5e121bbbb5489d9369af4bbabf5579542c55
chdzr <chidzir.wicaksono@gmail.com>
2021-01-21 22:31:51 +0700

    Tugas 2 - Modify with function implementation

--- Tugas-2/Soal-1.py
+++ Tugas-2/Soal-1.py
@@ -1,38 +1,79 @@
-list_contact = []
-welcome_text = "Selamat Datang!"
-menu_not_found = "Menu Tidak ditemukan"
+import os
 
-show_menu = True
-itteration_menu = 0
+#clear terminal console
+def clear_console():
+    os.system('cls')
 
-while show_menu:
-    if itteration_menu == 0 :
-        print(welcome_text)
-    print("Silahkan Masukkan Menu")
+def show_welcome_board():
+    print("Selamat datang!")
+
+def show_main_menu():
+    print("--- Menu ---")
     print("1. Daftar Kontak")
-    print("2. Input Kontak")
+    print("2. Tambah Kontak")
     print("3. Keluar")
+    print()
     print("pilih Menu :")
-    
-    option = int(input())
 
-    itteration_menu += 1
+def show_exit_message():
+    print("Program selesai, sampai jumpa!")
 
-    if option == 3:
-        print("Program Telah Selesai, terimakasih")
-        show_menu = False
-    elif option == 2:
-        contact = []
-        name= input("Masukkan Nama :")
-        phone= input("Masukkan No Telp :")
-        contact.append(name)
-        contact.append(phone)
-        list_contact.append(contact)
-    elif option == 1:
-        print("List of Contact :")
-        for i in range(len(list_contact)):
-            contact = list_contact[i]
-            print("Nama : {}".format(contact[0]))
-            print("No Telp : {}".format(contact[1]))
-    else:
-        print("input tidak diketahui")
+def show_unexpected_input():
+    print("Menu Tidak Tersedia")
+    prompt_back_to_menu()
+
+def show_contact_added_message():
+    print("Kontak berhasil ditambahkan")
+
+def entry_contact_list(list_contact):
+    contact = {"name":"","phone":""}
+    contact["name"]= input("Masukkan Nama :")
+    contact["phone"]= input("Masukkan No Telp :")
+    list_contact.append(contact)
+    show_contact_added_message()
+    print()
+    prompt_back_to_menu()
+
+def show_contact_list(list_contact):
+    print("Daftar kontak :")
+    for i in range(len(list_contact)):
+        contact = list_contact[i]
+        print("Nama: {}".format(contact["name"]))
+        print("No Telepon: {}".format(contact["phone"]))
+        print()
+    prompt_back_to_menu()
+
+def prompt_back_to_menu():
+    input("Tekan Enter untuk kembali ke menu")
+    clear_console()
+
+def main():
+    list_contact = []
+    show_menu = True
+    itteration_menu = 0
+    while show_menu:
+        if itteration_menu == 0 :
+            show_welcome_board()
+        show_main_menu()
+        #try-except for prevent breaking code while input alphabetical character
+        try:
+            option = int(input())
+        except:
+            option = 4
+        
+        clear_console()
+        
+        if option == 3:
+            show_exit_message()
+            show_menu = False
+        elif option == 2:
+            entry_contact_list(list_contact)
+        elif option == 1:
+            show_contact_list(list_contact)
+        else:
+            show_unexpected_input()
+            
+
+
+if __name__ == "__main__":
+    main()
